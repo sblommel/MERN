d8a9b67b86fa595f9678b6681dc447487f23cc1c
Samira Blommel <sjblommel@gmail.com>
2022-01-15 21:28:20 -0500

    Box Generator

---
+++ React/boxes/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Box from './components/Box'
+
+function App() {
+  return (
+    <div className="App container">
+      <h2>Box Generator</h2>
+      <Box></Box>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/boxes/src/components/Box.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import styles from './Box.module.css';
+
+const Box = (props)=>{
+
+    let [boxColor, setColor] = useState ("");
+
+    let [listOfBoxes, setListOfBoxes] = useState([]);
+
+    const createBox = (e)=>{
+        e.preventDefault();
+        // Prevent page for reloading line 8
+        let boxObj = {boxColor:boxColor}
+
+        setListOfBoxes([...listOfBoxes,boxObj])
+    }
+
+    return(
+    <>
+        <form onSubmit = {createBox}>
+            <div clasName="form-group">
+                <label htmlFor="">Box Color:<input type="text" name="" id="" className="form-control" onChange = {(event)=>setColor(event.target.value)}/> </label>
+                
+                
+            </div>
+            <input type="submit" value="Add box"/>
+        </form>
+        
+        
+        {
+            listOfBoxes.map((box)=>{
+                return (
+                    <div className= {styles.box} style = {{backgroundColor: box.boxColor,}}>
+                         <p>{box.boxColor}</p>
+                         
+                    </div>
+                        
+                )
+            })
+        }
+        
+    )
+
+    </>        
+    )}
+
+export default Box;
