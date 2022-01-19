a1adfb4eaa3317cb3a9936a3bdcb588e865dea57
Samira Blommel <sjblommel@gmail.com>
2022-01-18 19:11:57 -0500

    Pokemon Axios API

--- React/pokemon/src/App.js
+++ React/pokemon/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Pokemon from './components/Pokemon'
+import PokemonAxios from './components/PokemonAxios';
 
 function App() {
   return (
     <div className="App">
       <h1>Pokemon API</h1>
-      <Pokemon></Pokemon>
+      {/* <Pokemon></Pokemon> */}
+      <PokemonAxios></PokemonAxios>
     </div>
   );
 }
---
+++ React/pokemon/src/components/PokemonAxios.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const PokemonAxios = ()=>{
+
+    let [listOfPokemon, setListOfPokemon] = useState ([])
+
+    const getPokemon = ()=>{
+        console.log("You clicked this button");
+
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=1000")
+        
+            .then(response=>{
+            console.log("Response from API", response.data)
+            setListOfPokemon(response.data.results)
+            // With axios need only 1 .then statement and we have to have response.data as well.
+        })
+
+        .catch(err=>{
+            console.log("error", err)
+        })
+    }
+
+    return(
+        <>
+        <h3>Catch'em All!</h3>
+        <p><button onClick = {getPokemon}>Fetch Pokemon</button></p>
+        
+        {
+            listOfPokemon.map((pokemonObj, i)=>{
+                return(
+                    <div key = {i}>
+                        <h3>{pokemonObj.name}</h3>
+
+                    </div>
+                )
+
+            })
+        }
+
+        </>
+    )
+}
+
+export default PokemonAxios;
